33b347253a5cb4baa93abac27e749091e3985517
SEA786 <shams.eltaf786@gmail.com>
2024-07-31 12:18:04 +0200

    02b / update

--- 02_SQL_Tables/02b_db_table_SQL.sql
+++ 02_SQL_Tables/02b_db_table_SQL.sql
@@ -15,17 +15,28 @@ CREATE DATABASE IF NOT EXISTS boo;
 USE boo;
 
 /* Tabelle test löschen, falls vorhanden*/
-
+DROP TABLE IF EXISTS test;
 
 /* Tab. test anlegen, falls noch nicht vorhanden*/
+CREATE TABLE IF NOT EXISTS test
+(
+	name VARCHAR(20) NOT NULL, "TBA"
+	age INT NOT NULL DEFAULT 0
+);
 
 
 /* Alle Tabellen in der DB anzeigen */
-#SHOW TABLES;
+SHOW TABLES;
 
 /* Struktur der Tabelle anzeigen */
-
+DESCRIBE TABLES;
 /* ----- Daten ------- */
+
+INSERT INTO test(name,age) VALUES ("Grizabella",29);
+INSERT INTO test(age,name) VALUES (35,"Alonzo");
+INSERT INTO test VALUES ();
+
+SELECT * FROM test;
 
 
 -- ABER: Doppelte Datensätze werden zugelassen !
