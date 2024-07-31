d465fd93ef42e3122937bede3e760f5a71581f41
SEA786 <shams.eltaf786@gmail.com>
2024-07-31 12:47:46 +0200

    02 / 02d update

--- 02_SQL_Tables/02d_db_table_SQL.sql
+++ 02_SQL_Tables/02d_db_table_SQL.sql
@@ -12,6 +12,12 @@
 DROP TABLE IF EXISTS boo.test;
 
 /* Tab. test anlegen, falls noch nicht vorhanden*/
+CREATE TABLE IF NOT EXISTS boo.test
+(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL DEFAULT "TBA",
+	age INT NOT NULL DEFAULT 0
+);
 
 
 /* Alle Tabellen in der DB anzeigen */
@@ -21,6 +27,11 @@ DROP TABLE IF EXISTS boo.test;
 DESCRIBE boo.test;
 
 /* ----- Daten ------- */
+INSERT INTO boo.test(name,age) VALUES ("Grizabella",29);
+INSERT INTO test VALUES ();
+INSERT INTO boo.test(age,name) VALUES (35,"Alonzo");
+INSERT INTO boo.test(age,name) VALUES (35,"Alonzo");
+INSERT INTO boo.test(age,name) VALUES (35,"Alonzo, der Coole!");
 
 /* ---- Inhalte der Tabelle anzeigen ---- */
 SELECT * FROM boo.test;
