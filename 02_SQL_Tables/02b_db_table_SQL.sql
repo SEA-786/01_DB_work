47fd87e0050d85413e8cbcbc65611f4890cd7d14
SEA786 <shams.eltaf786@gmail.com>
2024-07-31 12:22:41 +0200

    02 / update

--- 02_SQL_Tables/02b_db_table_SQL.sql
+++ 02_SQL_Tables/02b_db_table_SQL.sql
@@ -20,26 +20,23 @@ DROP TABLE IF EXISTS test;
 /* Tab. test anlegen, falls noch nicht vorhanden*/
 CREATE TABLE IF NOT EXISTS test
 (
-	name VARCHAR(20) NOT NULL, "TBA"
+	name VARCHAR(20) NOT NULL DEFAULT "TBA",
 	age INT NOT NULL DEFAULT 0
 );
-
 
 /* Alle Tabellen in der DB anzeigen */
 SHOW TABLES;
 
 /* Struktur der Tabelle anzeigen */
-DESCRIBE TABLES;
-/* ----- Daten ------- */
+DESCRIBE test;
 
+/* ----- Daten ------- */
 INSERT INTO test(name,age) VALUES ("Grizabella",29);
 INSERT INTO test(age,name) VALUES (35,"Alonzo");
 INSERT INTO test VALUES ();
 
-SELECT * FROM test;
-
-
 -- ABER: Doppelte Datensätze werden zugelassen !
-
+INSERT INTO test(age,name) VALUES (35,"Alonzo");
 
 /* ---- Inhalte der Tabelle anzeigen ---- */
+SELECT * FROM test;
